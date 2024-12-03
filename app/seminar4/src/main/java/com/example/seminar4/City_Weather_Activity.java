7e3edb0aeed18ae05a28e09ed2d061e45da88c33
andreeascutaru <andreea.scutaru04@gmail.com>
2024-12-03 16:20:08 +0200

    committed

--- app/seminar4/src/main/java/com/example/seminar4/City_Weather_Activity.java
+++ app/seminar4/src/main/java/com/example/seminar4/City_Weather_Activity.java
@@ -1,11 +1,12 @@
 package com.example.seminar4;
 
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +15,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -27,6 +29,7 @@ import java.util.concurrent.Executors;
 
 public class City_Weather_Activity extends AppCompatActivity {
 
+    String text="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,45 +41,98 @@ public class City_Weather_Activity extends AppCompatActivity {
             return insets;
         });
 
+        Executor executor = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
 
-        Button btn = findViewById(R.id.butonVreme);
-        btn.setOnClickListener(new View.OnClickListener() {
+        EditText etOras = findViewById(R.id.orasET);
+        Button btnVremeOras = findViewById(R.id.butonVreme);
+        btnVremeOras.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String city = etOras.getText().toString();
+                String apiKey = "y88aaM0zx6oy5OALHpNZNcPbZ5VogWy8";
                 String link = "http://dataservice.accuweather.com/locations/v1/cities/search?apikey=y88aaM0zx6oy5OALHpNZNcPbZ5VogWy8&q=";
-                Executor executor = Executors.newSingleThreadExecutor();
-                Handler handler = new Handler(Looper.myLooper());
+
                 executor.execute(new Runnable() {
                     @Override
                     public void run() {
-                        HttpURLConnection con = null;
+                        HttpURLConnection connection = null;
+                        BufferedReader reader =null;
                         try{
                             URL url = new URL(link);
-                            con = (HttpURLConnection)url.openConnection();
-                            InputStream is = con.getInputStream();
-                            InputStreamReader isr = new InputStreamReader(is);
-                            BufferedReader bufferedReader = new BufferedReader(isr);
-                            StringBuilder response = new StringBuilder();
-                            String line;
-                            while((line = bufferedReader.readLine())!=null){
-                                response.append(line);
-                            }
-                            //JSONArray vector = new JSONArray(response.toString());
-                        } catch (MalformedURLException e) {
-                            throw new RuntimeException(e);
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
-                        }
-                        handler.post(new Runnable() {
-                            @Override
-                            public void run() {
+                            connection = (HttpURLConnection)url.openConnection();
 
+                            connection.setRequestMethod("GET");
+                            connection.setConnectTimeout(10000);
+                            connection.setReadTimeout(10000);
+
+                            int responseCode = connection.getResponseCode();
+                            if(responseCode == HttpURLConnection.HTTP_OK){
+                                reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                                StringBuilder response = new StringBuilder();
+                                String line;
+                                while((line = reader.readLine())!=null){
+                                    response.append(line);
+                                }
+
+                                JSONArray jsonResponse = new JSONArray(response.toString());
+                                JSONObject object = jsonResponse.getJSONObject(0);
+                                String cityKey = object.getString("Key");
+
+                                String weatherURL = "http://dataservice.accuweather.com/forecasts/v1/daily/1day/"+cityKey+"?apikey=y88aaM0zx6oy5OALHpNZNcPbZ5VogWy8&metric=true";
+                                try{
+                                    URL url2 = new URL(weatherURL);
+                                    connection = (HttpURLConnection)url2.openConnection();
+                                    int responseCode2 = connection.getResponseCode();
+                                    if(responseCode2==HttpURLConnection.HTTP_OK){
+                                        reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                                        StringBuilder response2 = new StringBuilder();
+                                        String line2;
+                                        while((line2 = reader.readLine())!=null){
+                                            response2.append(line2);
+                                        }
+                                        JSONObject object1 = new JSONObject(response2.toString());
+                                        JSONArray array1 = object1.getJSONArray("DailyForecasts");
+
+                                        JSONObject object2 = array1.getJSONObject(0);
+                                        JSONObject object3 = object2.getJSONObject("Temperature");
+                                        JSONObject minimum = object3.getJSONObject("Minimum");
+                                        JSONObject maximum = object3.getJSONObject("Maximum");
+                                        String valueMin = minimum.getString("Value");
+                                        String valueMax = maximum.getString("Value");
+
+                                        text = "Temperatura minima: "+valueMin+" Temperatura maxima: "+valueMax;
+                                    }
+                                }finally {
+                                    handler.post(() -> {
+                                        TextView cityTextView = findViewById(R.id.OrasVremeTV);
+                                        cityTextView.setText(text);
+                                    });
+
+                                }
                             }
-                        });
+                        } catch (Exception e){
+                            handler.post(()->{
+                                TextView cityTextView = findViewById(R.id.OrasVremeTV);
+                                cityTextView.setText("Error "+e.getMessage());
+                            });
+                        }finally{
+                            if(reader!=null){
+                                try{
+                                    reader.close();
+                                }catch(Exception ignored){}
+                            }
+                            if(connection!=null){
+                                connection.disconnect();
+                            }
+                        }
+
                     }
                 });
             }
         });
+
+
 
     }
 }
